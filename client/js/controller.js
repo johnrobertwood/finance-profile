010ea644883953ee9a1f7e3379e20d9a356c6c60
John Wood <johnrobertwood@yahoo.com>
2016-05-09 19:35:01 -0700

    Removing angular  and going to single page

--- client/js/controller.js
+++ client/js/controller.js
@@ -1,32 +1,49 @@
-angular.module('financeController', ['ngRoute']).
+angular.module('financeController', []).
 
-controller('ProfileCtrl', ['$rootScope', '$scope', '$http', '$location', function($rootScope, $scope, $http, $location) {
+controller('ProfileCtrl', ['$scope', '$http', '$location', function($scope, $http, $location) {
+  
+  $scope.showLogin = true;
+  $scope.showProfile = false;
+  $scope.showEdit = false;
 
-	$scope.userLogin = function() {	
-		$http.post('/login', $scope.loginData).success(function(res) {
-			$rootScope.user = res;
-			if(res.success !== false) {
-				$location.path('/profile')
-			}
-		});
-	};
+  $scope.userLogin = function() { 
+    $http.post('/login', $scope.loginData).success(function(res) {
+      $scope.user = res;
+      if(res.success !== false) {
+        $scope.showLogin = false
+        $scope.showProfile = true
+        $location.path('/login')
+      }
+    });
+  };
 
-	$scope.editDetails = function() {
-		$location.path('/profile/edit')
-	}	
+  $scope.updateProfile = function() {
+    $http.post('/profile/update', $scope.user).success(function(res) {
+      $scope.user = res;
+      if(res.success !== false) {
+        $scope.showEdit = false;
+        $scope.showProfile = true;
+        $location.path('/profile/update')
+      }
+    })
+  }
 
-	$scope.userLogout = function() {
-		$rootScope.user = {}
-		$location.path('/login')
-	}
+  $scope.editDetails = function() {
+    $http.post('/profile/edit', $scope.user).success(function(res) {
+      $scope.user = res;
+      if(res.success !== false) {
+        $scope.showProfile = false
+        $scope.showEdit = true
+        $location.path('/profile/edit')
+      }
+    })
+  }
 
-	$scope.updateProfile = function() {
-		$http.post('/profile/update', $scope.user).success(function(res) {
-			$rootScope.user = res;
-			if(res.success !== false) {
-				$location.path('/profile')
-			}
-		})
-	}
+  $scope.userLogout = function() {
+    $scope.user = {}
+    $scope.showProfile = false
+    $scope.showLogin = true;
+  }
+
 }]);
 
